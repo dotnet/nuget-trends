4df22fcc532955cb9eb515e59425b12188ddb559
Joao Grassi <joaop.grassi@gmail.com>
2018-08-25 20:49:19 +0200

    Redesigned the home page.
    Added the more or less final layout for the home page.

--- src/NuGetTrends.Spa/src/app/_layout/footer.component.ts
+++ src/NuGetTrends.Spa/src/app/_layout/footer.component.ts
@@ -2,13 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  template: `<footer class="footer">
-    <div class="content">
-      <div class="content has-text-centered">
-        NuGet Trends - All rights reserved
-      </div>
-    </div>
-  </footer>`
+  templateUrl: 'footer.component.html'
 })
 export class FooterComponent {
 }
