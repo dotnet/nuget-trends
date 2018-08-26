4d1f70e4c3dd4d5590463d561d1676c77679d0cb
Joao Grassi <joaop.grassi@gmail.com>
2018-08-26 16:30:43 +0200

    Created the initial version of the "packages type" component

---
+++ src/NuGetTrends.Spa/src/app/shared/components/search-type/search-type.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-type',
+  templateUrl: './search-type.component.html',
+  styleUrls: ['./search-type.component.scss']
+})
+export class SearchTypeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/NuGetTrends.Spa/src/app/shared/shared.module.ts
+++ src/NuGetTrends.Spa/src/app/shared/shared.module.ts
@@ -3,11 +3,12 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import {SearchInputComponent} from './components/search-input/search-input.component';
 import {PackageListComponent} from './components/package-list/package-list.component';
+import { SearchTypeComponent } from './components/search-type/search-type.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, ReactiveFormsModule],
-  declarations: [SearchInputComponent, PackageListComponent],
-  exports: [CommonModule, FormsModule, ReactiveFormsModule, SearchInputComponent, PackageListComponent]
+  declarations: [SearchInputComponent, PackageListComponent, SearchTypeComponent],
+  exports: [CommonModule, FormsModule, ReactiveFormsModule, SearchInputComponent, PackageListComponent, SearchTypeComponent]
 })
 export class SharedModule {
 }
