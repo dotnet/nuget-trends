eb8cc46f4e209e0d8c1f3838c764ec1e941ceb16
Bruno Garcia <bruno@brunogarcia.com>
2019-06-30 23:16:05 +0200

    write logs to logz.io

--- src/NuGetTrends.Scheduler/Program.cs
+++ src/NuGetTrends.Scheduler/Program.cs
@@ -1,28 +1,61 @@
 using System;
+using System.IO;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Sentry.Extensions.Logging;
+using Serilog;
+using SystemEnvironment = System.Environment;
 
 namespace NuGetTrends.Scheduler
 {
     public class Program
     {
-        public static void Main(string[] args)
+        private static readonly string Environment
+            = SystemEnvironment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+
+        public static IConfiguration Configuration { get; private set; } = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment}.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        public static int Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            if (Environment != "Production")
+            {
+                Serilog.Debugging.SelfLog.Enable(Console.Error);
+            }
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(Configuration)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting.");
+
+                CreateWebHostBuilder(args).Build().Run();
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseShutdownTimeout(TimeSpan.FromSeconds(30))
-                .ConfigureAppConfiguration((w, c) =>
-                {
-                    if (w.HostingEnvironment.IsDevelopment())
-                    {
-                        c.AddJsonFile("appsettings.local.json", optional: true, reloadOnChange: true);
-                    }
-                }).UseSentry(o => o.AddLogEntryFilter((category, level, eventId, exception)
+                .UseConfiguration(Configuration)
+                .UseSerilog()
+                .UseSentry(o => o.AddLogEntryFilter((category, level, eventId, exception)
                                     => eventId.ToString() ==
                                        "Microsoft.EntityFrameworkCore.Infrastructure.SensitiveDataLoggingEnabledWarning"
                                        && string.Equals(
