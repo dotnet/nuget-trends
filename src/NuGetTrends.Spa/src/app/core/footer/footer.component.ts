9b7d80a92c40d9211d3ffe8ee9410ed1df7b01fb
Joao Grassi <joao@joaograssi.com>
2020-09-15 22:58:35 +0200

    Upgrade bulma

--- src/NuGetTrends.Spa/src/app/core/footer/footer.component.ts
+++ src/NuGetTrends.Spa/src/app/core/footer/footer.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  templateUrl: 'footer.component.html',
+  templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+
+  public year = new Date().getFullYear();
 }
