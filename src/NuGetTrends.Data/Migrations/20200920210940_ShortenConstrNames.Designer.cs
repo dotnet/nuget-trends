6c7da234d1bda70d11011e401c83ee41a62f6865
Joao Grassi <joao@joaograssi.com>
2020-09-20 23:17:44 +0200

    Fix pending migrations
    After upgrading to 3.x some long constraint appear in the migrations even though we didn't change them. Setting a fixed and shorter name to avoid EF doing anything with them.

--- src/NuGetTrends.Data/Migrations/20200920210940_ShortenConstrNames.Designer.cs
+++ src/NuGetTrends.Data/Migrations/20200920210940_ShortenConstrNames.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using NuGetTrends.Data;
@@ -10,9 +11,10 @@ using NuGetTrends.Data;
 namespace NuGetTrends.Data.Migrations
 {
     [DbContext(typeof(NuGetTrendsContext))]
-    partial class NuGetTrendsContextModelSnapshot : ModelSnapshot
+    [Migration("20200920210940_ShortenConstrNames")]
+    partial class ShortenConstrNames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
