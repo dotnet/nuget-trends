62a4654faec95de834fd767e0399cd34498e8e6b
Joao Grassi <joao@joaograssi.com>
2020-12-21 20:32:53 +0100

    Rename to Web This project will also now host the SPA

--- src/NuGetTrends.Web/ExceptionInResponseMiddleware.cs
+++ src/NuGetTrends.Web/ExceptionInResponseMiddleware.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 
-namespace NuGetTrends.Api
+namespace NuGetTrends.Web
 {
     // ReSharper disable once ClassNeverInstantiated.Global - reflection
     internal class ExceptionInResponseMiddleware
