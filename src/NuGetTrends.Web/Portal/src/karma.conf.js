ffc690b3b49f09bee1fb99150ba2724f98b55fe8
Joao Grassi <joao@joaograssi.com>
2020-12-24 14:06:22 +0100

    Fix warning with karma istanbul

--- src/NuGetTrends.Web/Portal/src/karma.conf.js
+++ src/NuGetTrends.Web/Portal/src/karma.conf.js
@@ -12,16 +12,21 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly', 'text-summary', 'cobertura'],
-      fixWebpackSourcePaths: true
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'lcovonly' },
+        { type: 'text-summary' },
+        { type: 'cobertura' }
+      ],
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
