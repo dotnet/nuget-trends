99e182ee421bc5056cc5516d2235e43250cb3e67
Catherine Lee <55311782+c298lee@users.noreply.github.com>
2023-12-13 13:48:58 -0500

    add experimental canvas recording for replays

--- src/NuGetTrends.Web/Portal/src/app/app.module.ts
+++ src/NuGetTrends.Web/Portal/src/app/app.module.ts
@@ -8,6 +8,7 @@ import * as Sentry from '@sentry/angular-ivy';
 import { Replay } from "@sentry/replay";
 import { HttpClient, CaptureConsole, ReportingObserver } from "@sentry/integrations";
 import { Feedback } from '@sentry-internal/feedback';
+import { getCanvasManager } from '@sentry-internal/rrweb';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routes.module';
@@ -34,6 +35,13 @@ Sentry.init({
       networkDetailAllowUrls: environment.NETWORK_DETAIL_ALLOW_URLS,
       networkRequestHeaders: ["referrer", "sentry-trace", "baggage"],
       networkResponseHeaders: ["Server"],
+       _experiments: {
+        canvas: {
+          fps: 4,
+          quality: 0.6,
+          manager: getCanvasManager,
+        },
+      },
     }),
     new Sentry.BrowserTracing({
       routingInstrumentation: Sentry.instrumentAngularRouting,
